a0d4146b7bf59cc20a5468239f36ef9bbe444f5c
Ishaan Bajpai <ishaanbajpai1@gmail.com>
2021-04-30 14:09:30 +0530

    added seperate BlogLists component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogsList from "./BlogsList";
 
     const Home = () => {
         const [blogs, setBlogs] = useState([
@@ -8,12 +9,8 @@ import { useState } from "react";
           ]);
     return (  
         <div className="home">
-            {blogs.map((blog)=>(
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <BlogsList blogs = {blogs} title = "All Blogs!"></BlogsList>
+            <BlogsList blogs = {blogs.filter((blog)=> blog.author === 'mario')} title = "Mario's Blogs!"></BlogsList>
         </div>
     );
 }
