b67d60f4d65e2015de205dc74843d4207f386bba
Ishaan Bajpai <ishaanbajpai1@gmail.com>
2021-05-08 15:35:49 +0530

    custom hook for fetch req

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,14 @@
 import { useState, useEffect } from "react";
 import BlogsList from "./BlogsList";
+import useFetch from "./useFetch";
 
     const Home = () => {
-        const [blogs, setBlogs] = useState(null);
-    useEffect(()=>{
-        fetch("http://localhost:8000/blogs")
-        .then(res => {
-            return res.json();
-        })
-        .then(data =>{
-            setBlogs(data);
-        })
-    }, []);
+        const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+    
     return (  
         <div className="home">
+            {error && <p>{error}</p>}
+            {isPending && <p>Loading Blogs...</p>}
             { blogs && <BlogsList blogs = {blogs} title = "All Blogs!"></BlogsList>}
         </div>
     );
